e82a179cda715b1da6d4ab4bc2b08c0b9c1c4841
Heloísa Raquel Siebeneichler <124800380+heloisars2@users.noreply.github.com>
2023-08-21 09:58:44 -0300

    Add files via upload

---
+++ animaisfofinhos.js
@@ -0,0 +1,30 @@
+const img_dog = document.getElementById("doguinho");
+
+async function pesquisarDoguinho() {
+    const url = `https://api.thedogapi.com/v1/images/search`;
+    const deucerto= await fetch(url);
+
+    const data = await deucerto.json();
+    console.log(data[0].url)
+
+
+    const new_url = data[0].url;
+    img_dog.src = new_url;
+
+}
+document.getElementById("buttondog").addEventListener("click", pesquisarDoguinho);
+
+async function pesquisarGatinho() {
+    const url = `https://api.thecatapi.com/v1/images/search`;
+    const deucerto= await fetch(url);
+
+    const data = await deucerto.json();
+    console.log(data[0].url)
+
+
+    const new_url = data[0].url;
+    img_cat.src = new_url;
+
+}
+const img_cat = document.getElementById("gatinho");
+document.getElementById("buttoncat").addEventListener("click", pesquisarGatinho);
